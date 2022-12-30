def10f9a08574a8d07bdfbbe773afd7756d3cff6
ifdunctor <helloworld@aviral.me>
2022-12-31 00:53:59 +0100

    Replace hero kinetic text animation with text distortion

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,47 +1,38 @@
-import { JSXElement, onCleanup, onMount } from "solid-js";
-
-import { createSignal } from "solid-js";
-import { Rerun } from "@solid-primitives/keyed";
-import { Motion, Presence } from "@motionone/solid";
+import type { JSXElement } from "solid-js";
 
 import "./hero.scss";
 
-const adjectives = [
-  'Passionate',
-  'Meticulous',
-  'Mediocre  ',
-]
+// const adjectives = ["Passionate", "Meticulous", "Mediocre  "];
 
-const nouns = [
-  'Engineer ',
-  'Developer',
-  'Artist   ',
-]
+// const nouns = ["Engineer ", "Developer", "Artist   "];
 
 export function Hero(): JSXElement {
-  const [word, setWord] = createSignal(0);
+  // const [word, setWord] = createSignal(0);
 
-  let timer: NodeJS.Timer | null = null; 
+  // let timer: NodeJS.Timer | null = null;
 
-  onMount(() => {
-    if (!timer) {
-      timer = setInterval(() => {
-        setWord((p) => p === 2 ? 0 : p + 1);
-      }, 6000);
-    }
-  })
+  // onMount(() => {
+  //   if (!timer) {
+  //     timer = setInterval(() => {
+  //       setWord((p) => (p === 2 ? 0 : p + 1));
+  //     }, 4200);
+  //   }
+  // });
 
-  onCleanup(() => {
-    if (timer) {
-      clearInterval(timer);
-    }
-  });
+  // onCleanup(() => {
+  //   if (timer) {
+  //     clearInterval(timer);
+  //   }
+  // });
 
   return (
     <div class="HOMEHERO relative h-screen">
-      <span class="HOMEHERO_hero-title-row flex justify-between text-8xl">
-        <div class="top-text absolute">
-          {adjectives?.[0]?.split('').map((_, k) => (
+      <span
+        class="HOMEHERO_hero-title-row flex justify-between text-8xl"
+        id="hero-container"
+      >
+        {/* <div class="top-text absolute">
+          {adjectives?.[0]?.split("").map((_, k) => (
             <Presence exitBeforeEnter>
               <Rerun on={word()}>
                 <Motion.span
@@ -49,7 +40,11 @@ export function Hero(): JSXElement {
                   initial={{ opacity: 0, scale: 0.6, y: -50 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
                   exit={{ opacity: 0, scale: 0.6, y: -50 }}
-                  transition={{ duration: 0.1, delay: k * 0.03, easing: 'ease-in-out' }}
+                  transition={{
+                    duration: 0.1,
+                    delay: k * 0.03,
+                    easing: "ease-in-out",
+                  }}
                 >
                   {(adjectives[word()] as string)[k]}
                 </Motion.span>
@@ -59,7 +54,7 @@ export function Hero(): JSXElement {
         </div>
 
         <div class="bottom-text absolute">
-        {nouns?.[0]?.split('').map((_, k) => (
+          {nouns?.[0]?.split("").map((_, k) => (
             <Presence exitBeforeEnter>
               <Rerun on={word()}>
                 <Motion.span
@@ -67,18 +62,19 @@ export function Hero(): JSXElement {
                   initial={{ opacity: 0, scale: 0.6, y: 80 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
                   exit={{ opacity: 0, scale: 0.6, y: 80 }}
-                  transition={{ duration: 0.1, delay: (k * 0.01) + 0.25, easing: 'ease-out' }}
+                  transition={{
+                    duration: 0.1,
+                    delay: k * 0.01 + 0.25,
+                    easing: "ease-out",
+                  }}
                 >
                   {(nouns[word()] as string)[k]}
                 </Motion.span>
               </Rerun>
             </Presence>
           ))}
-        </div>
+        </div> */}
       </span>
-      <div class="astrx">
-        <img src="/creatives/astrx.svg" />
-      </div>
       <div class="circle">
         <div class="logo"></div>
         <div class="text">
